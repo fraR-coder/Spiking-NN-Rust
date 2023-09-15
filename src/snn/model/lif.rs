a2432ae3b65c987ada2981404f9819cfdf5dcb03
fraR-coder <francesco.mail5.0@gmail.com>
2023-09-15 13:26:32 +0200

    added struct for configuration and implementations

--- src/snn/model/lif.rs
+++ src/snn/model/lif.rs
@@ -12,14 +12,50 @@ pub struct LifNeuron {
     /// Membrane's time constant. This is the product of its capacity and resistance
     pub tau: f64,
     /// Flag to mark bit change
-    pub stuck_at_zero: bool
+    pub stuck_at_zero: bool,
+
+    pub v_mem: f64,
+    pub ts_old: u128,
+}
+/// A struct used to create a specific configuration, simply reusable for other neurons
+
+#[derive(Clone, Debug)]
+pub struct Configuration {
+    v_rest: f64,
+    v_reset: f64,
+    v_threshold: f64,
+    tau: f64,
 }
 
-impl From<&LifNeuron> for LifSolverVars {
-    fn from(neuron: &LifNeuron) -> Self {
-        Self {
-            v_mem: neuron.v_rest,
-            ts_old: 0
+// IMPLEMENTATION FOR LIF NEURONS & LIF NEURON CONFIG
+
+impl LifNeuron {
+    pub fn new(v_rest: f64, v_reset: f64, v_threshold: f64, tau: f64) -> LifNeuron {
+        LifNeuron {
+            // parameters
+            v_rest,
+            v_reset,
+            v_threshold,
+            tau,
+            v_mem: 0.0, //inizlamente a 0?
+            ts_old: 0,
+        }
+    }
+
+    pub fn from_conf(nc: &Configuration) -> LifNeuron {
+        Self::new(nc.v_rest, nc.v_reset, nc.v_threshold, nc.tau)
+    }
+}
+
+impl Configuration {
+    /// Create a new Configuration, which can be used to build one or more identical neurons.
+
+    pub fn new(v_rest: f64, v_reset: f64, v_threshold: f64, tau: f64) -> Configuration {
+        Configuration {
+            v_rest,
+            v_reset,
+            v_threshold,
+            tau,
         }
     }
 }
