9d3ad6b81ac106d045494346125c3c482b4c9c42
Nico331 <slav.nicolai@mail.ru>
2023-09-22 10:08:28 +0200

    new struct layer

---
+++ src/snn/layer.rs
@@ -0,0 +1,19 @@
+use std::ops::{Index, IndexMut};
+use na::{DMatrix, Vector3};
+use crate::Model;
+
+
+/// A single layer in the neural network
+///
+/// This contains all the neurons of the layer, as well as the intra-layer weights and input weights from
+/// the previous layer.
+#[derive(Clone)]
+pub struct Layer<M: Model> {
+    /// List of all neurons in this layer
+    pub(crate) neurons: Vec<M::Neuron>,
+    /// Matrix of the input weights. For the first layer, this must be a square diagonal matrix.
+    pub(crate) input_weights: DMatrix<f64>,
+    /// Square matrix of the intra-layer weights
+    pub(crate) intra_weights: DMatrix<f64>
+}
+
