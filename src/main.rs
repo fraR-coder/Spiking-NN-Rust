f71344193dadae9cf96c1f71d30a7b6d006fcd3a
fraR-coder <francesco.mail5.0@gmail.com>
2023-09-22 12:35:27 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use snn::layer::*;
 
 
 use snn::layer::*;
-use crate::snn::nn::NNBuilder;
+use crate::snn::nn::NN;
 fn main() {
     let config=Configuration::new(1.0,2.0,3.0,4.0);
     let mut neuron1=LifNeuron::from_conf(&config);
@@ -19,7 +19,7 @@ fn main() {
     let ts=1;
 
     let le:Layer<LeakyIntegrateFire>=Layer::new(neuron2);
-    let nn = NNBuilder::<LeakyIntegrateFire>::new();
+    let nn = NN::<LeakyIntegrateFire>::new();
 
     println!("res: {}", LeakyIntegrateFire::handle_spike(&mut neuron1, weighted_input_val, ts));
 
